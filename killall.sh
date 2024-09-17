f0e2e889a827ae1e7d2b305665d7e48dc32ea2c9
Robin Montfermeé <robin.montferme@u-bordeaux.fr>
2024-09-17 01:20:04 -0400

    add a cleanup script

---
+++ killall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pkill  ossia-score
+pkill  aes67-daemon
+pkill  jackd
+pkill  open-stage-control
+
+pkill -9 ossia-score
+pkill -9 aes67-daemon
+pkill -9 jackd
+pkill -9 open-stage-control
