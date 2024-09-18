31c11c8bc9b9c6fc1e3e8df4edb897895629e8b6
Robin Montfermeé <robin.montferme@u-bordeaux.fr>
2024-09-18 01:23:20 -0400

    save

--- killall.sh
+++ killall.sh
@@ -2,9 +2,9 @@
 pkill  ossia-score
 pkill  aes67-daemon
 pkill  jackd
-pkill  open-stage-control
+pkill  open-stage-c
 
 pkill -9 ossia-score
 pkill -9 aes67-daemon
 pkill -9 jackd
-pkill -9 open-stage-control
+pkill -9 open-stage-c
